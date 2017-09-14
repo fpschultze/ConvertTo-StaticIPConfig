8db5742b624beeec8ce57b5b941d9534ac406561
Frank Peter Schultze <fpschultze@live.de>
2017-09-14 17:12:33 +0200

    First commit

---
+++ ConvertTo-StaticIPConfig.ps1
@@ -0,0 +1,12 @@
+﻿function ConvertTo-StaticIPConfig
+{
+  Get-WmiObject -Class Win32_NetworkAdapterConfiguration |
+    Where-Object {$_.IPEnabled -and $_.DHCPEnabled} |
+      ForEach-Object
+      {
+        $_.SetDNSServerSearchOrder($_.DNSServerSearchOrder)
+        $_.SetDynamicDNSRegistration('TRUE')
+        $_.EnableStatic($_.IPAddress[0], $_.IPSubnet[0])
+        $_.SetGateways($_.DefaultIPGateway)
+      }
+}
